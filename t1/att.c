de6a47b294ccc4e346e5b7bbd5bfc3895cd348fd
Daniele <carvalho.danielekr@gmail.com>
2024-08-23 09:51:44 -0300

    add comentarios

--- t1/att.c
+++ t1/att.c
@@ -2,16 +2,16 @@
 #include <stdlib.h>
 #include "att.h"
 
-void encontraAtt(int idArquivo, Atributo** atts, int* n){
-    FILE* file = fopen("t1/att.dat", "rb");
+void encontraAtt(int idArquivo, Atributo** atts, int* n){ //recebe o id do arquivo, um ponteiro para um vetor de atributos
+    FILE* file = fopen("t1/att.dat", "rb"); //leitura binária
     if(file == NULL){
         return;
     }
     Atributo att;
-    while(fread(&att, sizeof(Atributo), 1, file)){
+    while(fread(&att, sizeof(Atributo), 1, file)){ //lê o arquivo e armazena na struct att
         if(att.id_arquivo_att == idArquivo){
             *n += 1;
-            *atts = (Atributo*)realloc(*atts, *n * sizeof(Atributo));
+            *atts = (Atributo*)realloc(*atts, *n * sizeof(Atributo)); //realoca o vetor de atributos
             (*atts)[*n - 1] = att;
         }
     }
--- t1/table.c
+++ t1/table.c
@@ -2,13 +2,13 @@
 #include <string.h>
 #include "table.h"
 
-int entradaTabela(const char* nomeLogico, Tabela* tabela){
-    FILE* file = fopen("t1/table.dat", "rb");
+int entradaTabela(const char* nomeLogico, Tabela* tabela){ //recebe o buffer e o nome lógico da tabela
+    FILE* file = fopen("t1/table.dat", "rb"); //leitura binária
     if(file == NULL){
         return 0;
     }
-    while(fread(tabela, sizeof(Tabela), 1, file)){
-        if(strcmp(tabela->nome_logico, nomeLogico) == 0){
+    while(fread(tabela, sizeof(Tabela), 1, file)){ //lê o arquivo e armazena na struct tabela
+        if(strcmp(tabela->nome_logico, nomeLogico) == 0){  //compara o nome lógico da tabela com o nome lógico passado
             fclose(file);
             return 1;
         }
@@ -17,7 +17,7 @@ int entradaTabela(const char* nomeLogico, Tabela* tabela){
     return 0;
 }
 
-void printTabela(const char* nomeLogico, Atributo* atts, int n){
+void printTabela(const char* nomeLogico, Atributo* atts, int n){ //recebe o nome lógico da tabela
     Tabela tabela;
     if(entradaTabela(nomeLogico, &tabela)){
         printf("Tabela: %s\n", tabela.nome_fisico);
